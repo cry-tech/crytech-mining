239ab2e8657d0b61df29113455d1f6e75aa9f32b
ali gonabadi <aligonabadi@live.com>
2019-12-24 18:44:04 +0330

    few javascript bugs fixed

--- website/js/light-bootstrap-dashboard.js
+++ website/js/light-bootstrap-dashboard.js
@@ -22,92 +22,90 @@ var fixedTop = false;
 
 var navbar_initialized = false;
 
-$(document).ready(function(){
-    window_width = $(window).width();
+$(document).ready(function () {
+  window_width = $(window).width();
 
-    // check if there is an image set for the sidebar's background
-    lbd.checkSidebarImage();
+  // check if there is an image set for the sidebar's background
+  lbd.checkSidebarImage();
 
-    // Init navigation toggle for small screens
-    lbd.initRightMenu();
+  // Init navigation toggle for small screens
+  lbd.initRightMenu();
 
-    //  Activate the tooltips
-    $('[rel="tooltip"]').tooltip();
+  //  Activate the tooltips
+  $('[rel="tooltip"]').tooltip();
 
-    $('.form-control').on("focus", function(){
-        $(this).parent('.input-group').addClass("input-group-focus");
-    }).on("blur", function(){
-        $(this).parent(".input-group").removeClass("input-group-focus");
+  $('.form-control').on("focus", function () {
+    $(this).parent('.input-group').addClass("input-group-focus");
+  }).on("blur", function () {
+    $(this).parent(".input-group").removeClass("input-group-focus");
+  });
+
+  // Fixes sub-nav not working as expected on IOS
+  $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+});
+
+$(document).on('click', '.navbar-toggle', function () {
+  $toggle = $(this);
+
+  if (lbd.misc.navbar_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+    lbd.misc.navbar_menu_visible = 0;
+    $('#bodyClick').remove();
+    setTimeout(function () {
+      $toggle.removeClass('toggled');
+    }, 550);
+  } else {
+    setTimeout(function () {
+      $toggle.addClass('toggled');
+    }, 580);
+    div = '<div id="bodyClick"></div>';
+    $(div).appendTo('body').click(function () {
+      $('html').removeClass('nav-open');
+      lbd.misc.navbar_menu_visible = 0;
+      setTimeout(function () {
+        $toggle.removeClass('toggled');
+        $('#bodyClick').remove();
+      }, 550);
     });
 
-    // Fixes sub-nav not working as expected on IOS
-$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+    $('html').addClass('nav-open');
+    lbd.misc.navbar_menu_visible = 1;
+  }
 });
 
-$(document).on('click', '.navbar-toggle', function(){
-    $toggle = $(this);
-
-    if(lbd.misc.navbar_menu_visible == 1) {
-        $('html').removeClass('nav-open');
-       lbd.misc.navbar_menu_visible = 0;
-        $('#bodyClick').remove();
-        setTimeout(function(){
-           $toggle.removeClass('toggled');
-       }, 550);
-    } else {
-       setTimeout(function(){
-           $toggle.addClass('toggled');
-       }, 580);
-       div = '<div id="bodyClick"></div>';
-       $(div).appendTo('body').click(function() {
-           $('html').removeClass('nav-open');
-           lbd.misc.navbar_menu_visible = 0;
-            setTimeout(function(){
-               $toggle.removeClass('toggled');
-               $('#bodyClick').remove();
-            }, 550);
-       });
-
-      $('html').addClass('nav-open');
-       lbd.misc.navbar_menu_visible = 1;
-    }
-});
-
-$(window).on('resize', function(){
-    if(navbar_initialized){
-        lbd.initRightMenu();
-        navbar_initialized = true;
-    }
+$(window).on('resize', function () {
+  if (navbar_initialized) {
+    lbd.initRightMenu();
+    navbar_initialized = true;
+  }
 });
 
 lbd = {
-    misc:{
-        navbar_menu_visible: 0
-    },
+  misc: {
+    navbar_menu_visible: 0
+  },
 
-    checkSidebarImage: function(){
-        $sidebar = $('.sidebar');
-        image_src = $sidebar.data('image');
+  checkSidebarImage: function () {
+    $sidebar = $('.sidebar');
+    image_src = $sidebar.data('image');
 
-        if(image_src !== undefined){
-            sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
-            $sidebar.append(sidebar_container);
-        }
-    },
+    if (image_src !== undefined) {
+      sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
+      $sidebar.append(sidebar_container);
+    }
+  },
 
-    initRightMenu: debounce(function(){
+  initRightMenu: debounce(function () {
 
-            if($(window).width() > 991){
-                // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
-                // $sidebar_wrapper.find('.navbar-form').remove();
-                $sidebar_wrapper.find('.nav-mobile-menu').remove();
+    if ($(window).width() > 991) {
+      // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
+      // $sidebar_wrapper.find('.navbar-form').remove();
+      //$sidebar_wrapper.find('.nav-mobile-menu').remove();
 
-                mobile_menu_initialized = false;
-            
-        }
-    },200)
+      mobile_menu_initialized = false;
+    }
+  }, 200)
 }
-
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
@@ -115,14 +113,14 @@ lbd = {
 // leading edge, instead of the trailing.
 
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
 };
